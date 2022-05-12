aa2eef1bf06ab366819d7c1e9b9a2875bc310579
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-12 12:43:13 -0300

    🥒 test: ajustes e mais passos

--- cypress/integration/app/elements.steps.js
+++ cypress/integration/app/elements.steps.js
@@ -1,5 +1,5 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
 
-Then(`eu vejo o título{string}`, title => {
+Then(`eu vejo o título {string}`, title => {
   cy.title().should("include", title);
 });
