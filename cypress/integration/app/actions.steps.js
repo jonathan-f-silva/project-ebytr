e12ad43069a5ce3d7a493409e91915745e8427ca
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-12 23:33:55 -0300

    🔧 config: cypress TS

--- cypress/integration/app/actions.steps.js
+++
@@ -1,7 +0,0 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
-
-const url = "https://localhost:3000";
-
-Given(`que eu entro no app`, () => {
-  cy.visit(url);
-});
--- cypress/integration/app/elements.steps.js
+++
@@ -1,5 +0,0 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
-
-Then(`eu vejo o título {string}`, title => {
-  cy.title().should("include", title);
-});
