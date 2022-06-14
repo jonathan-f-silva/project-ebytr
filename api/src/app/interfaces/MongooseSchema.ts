64eccc933d347032b52610bc89365eba74d30b0b
Jonathan Ferreira <j0n4t@hotmail.com>
2022-06-13 21:07:56 -0300

    ♻️ refactor: move API to submodule

--- api/src/app/interfaces/MongooseSchema.ts
+++
@@ -1,3 +0,0 @@
-import { SchemaDefinition, SchemaDefinitionType } from 'mongoose';
-
-export type MongooseSchema<T> = SchemaDefinition<SchemaDefinitionType<T>>;
--- api/src/routes/TodosRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Todo } from '../app/interfaces/TodoSchema';
-import ExpressRouter from './ExpressRouter';
-
-export default class TodosRouter extends ExpressRouter<Todo> {
-  public setupRoutes(): void {
-    super.setupRoutes();
-    this.router.patch(`${this.route}/:id/status`, this.update);
-  }
-}
